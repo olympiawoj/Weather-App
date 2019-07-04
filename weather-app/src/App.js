718faeebe088d25d8162cbcc01ac7027166dd24f
olympia <owojcik7@gmail.com>
2019-07-04 19:22:20 -0400

    Test CDM running in App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import WeatherSummary from "./components/WeatherSummary";
 
 class App extends Component {
+  state = {
+    weatherData: []
+  };
+
+  componentDidMount() {
+    console.log("App.js component did Mount ran");
+  }
   render() {
     return (
       <div>
