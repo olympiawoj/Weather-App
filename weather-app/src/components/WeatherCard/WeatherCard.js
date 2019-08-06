3590adde1ba1c0eb0e6fbb77056460bb72cd20c1
olympia <owojcik7@gmail.com>
2019-08-05 23:09:55 -0400

    Render weather icons dynamically

--- weather-app/src/components/WeatherCard/WeatherCard.js
+++ weather-app/src/components/WeatherCard/WeatherCard.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./WeatherCard.css";
 
-const iconMap = {
-  "clear sky": "01d"
-};
-
 const WeatherCard = props => {
+  const iconMap = {
+    "clear sky": "01d",
+    "few clouds": "02d"
+  };
+
+  let url = null;
+  if (props.weatherDes in iconMap) {
+    let dynamic = iconMap[props.weatherDes];
+    url = `http://openweathermap.org/img/wn/${dynamic}@2x.png`;
+  }
+
   console.log("Weather Card props", props);
   //ideally img src is dynamic, pointing to the image
   return (
     <div className="weather-card">
       {props.date}
       <div>{props.weatherDes}</div>
-
-      <img src="http://openweathermap.org/img/wn/11d@2x.png" />
+      <img src={url} />
     </div>
   );
 };
