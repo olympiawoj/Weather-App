d716bc6068def60916e9aabb96cc7decc0228fe1
olympia <owojcik7@gmail.com>
2019-08-05 23:05:00 -0400

    Access weather description and start building icon map to retrieve icon for images

--- weather-app/src/components/WeatherCard/WeatherCard.js
+++ weather-app/src/components/WeatherCard/WeatherCard.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./WeatherCard.css";
 
+const iconMap = {
+  "clear sky": "01d"
+};
+
 const WeatherCard = props => {
   console.log("Weather Card props", props);
+  //ideally img src is dynamic, pointing to the image
+  return (
+    <div className="weather-card">
+      {props.date}
+      <div>{props.weatherDes}</div>
 
-  return <div className="weather-card"> {props.date} </div>;
+      <img src="http://openweathermap.org/img/wn/11d@2x.png" />
+    </div>
+  );
 };
 
 export default WeatherCard;
--- weather-app/src/components/WeatherSummary/WeatherSummary.js
+++ weather-app/src/components/WeatherSummary/WeatherSummary.js
@@ -4,13 +4,23 @@ import WeatherCard from "../WeatherCard/WeatherCard";
 import "./WeatherSummary.css";
 
 const WeatherSummary = props => {
+  const iconMap = {
+    "clear sky": "01d",
+    "few clouds": "02d"
+  };
+
   console.log({ props });
+
   return (
     <>
       WeatherSummary
       <div className="weather-summary">
         {props.weatherData.map((card, index) => (
-          <WeatherCard date={card.dt_txt} key={index} />
+          <WeatherCard
+            date={card.dt_txt}
+            key={index}
+            weatherDes={card.weather[0].description}
+          />
         ))}
       </div>
     </>
